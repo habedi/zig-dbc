ae6c993874627211abdd3f239306f90abc1e78c2
Hassan Abedi <hassan.abedi.t@gmail.com>
2025-10-03 22:07:35 +0200

    Fix the missing `examples` dir bug

--- build.zig
+++ build.zig
@@ -49,32 +49,36 @@ pub fn build(b: *std.Build) void {
 
     // --- Example Setup ---
     const examples_path = "examples";
-    var examples_dir = fs.cwd().openDir(examples_path, .{ .iterate = true }) catch |err| {
-        if (err == error.FileNotFound) return;
-        @panic("Can't open 'examples' directory");
-    };
-    defer examples_dir.close();
+    examples_blk: {
+        // If the examples directory isn't present (common when used as a dependency),
+        // skip setting up example artifacts instead of panicking.
+        var examples_dir = fs.cwd().openDir(examples_path, .{ .iterate = true }) catch |err| {
+            if (err == error.FileNotFound or err == error.NotDir) break :examples_blk;
+            @panic("Can't open 'examples' directory");
+        };
+        defer examples_dir.close();
 
-    var dir_iter = examples_dir.iterate();
-    while (dir_iter.next() catch @panic("Failed to iterate examples")) |entry| {
-        if (!std.mem.endsWith(u8, entry.name, ".zig")) continue;
+        var dir_iter = examples_dir.iterate();
+        while (dir_iter.next() catch @panic("Failed to iterate examples")) |entry| {
+            if (!std.mem.endsWith(u8, entry.name, ".zig")) continue;
 
-        const exe_name = fs.path.stem(entry.name);
-        const exe_path = b.fmt("{s}/{s}", .{ examples_path, entry.name });
+            const exe_name = fs.path.stem(entry.name);
+            const exe_path = b.fmt("{s}/{s}", .{ examples_path, entry.name });
 
-        const exe = b.addExecutable(.{
-            .name = exe_name,
-            .root_source_file = b.path(exe_path),
-            .target = target,
-            .optimize = optimize,
-        });
-        exe.root_module.addImport("dbc", lib_module);
-        b.installArtifact(exe);
+            const exe = b.addExecutable(.{
+                .name = exe_name,
+                .root_source_file = b.path(exe_path),
+                .target = target,
+                .optimize = optimize,
+            });
+            exe.root_module.addImport("dbc", lib_module);
+            b.installArtifact(exe);
 
-        const run_cmd = b.addRunArtifact(exe);
-        const run_step_name = b.fmt("run-{s}", .{exe_name});
-        const run_step_desc = b.fmt("Run the {s} example", .{exe_name});
-        const run_step = b.step(run_step_name, run_step_desc);
-        run_step.dependOn(&run_cmd.step);
+            const run_cmd = b.addRunArtifact(exe);
+            const run_step_name = b.fmt("run-{s}", .{exe_name});
+            const run_step_desc = b.fmt("Run the {s} example", .{exe_name});
+            const run_step = b.step(run_step_name, run_step_desc);
+            run_step.dependOn(&run_cmd.step);
+        }
     }
 }
